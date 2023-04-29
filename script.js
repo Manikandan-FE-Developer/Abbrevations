1138d45d7b8b4316c956dc2bf518f9a1d7c1ac84
Manikandan <122381569+Manikandan-3@users.noreply.github.com>
2023-04-29 17:56:51 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,174 @@
+function CPU(){
+	document.getElementById("CPU").innerHTML="Central Processing Unit";
+}
+function GPU(){
+	document.getElementById("GPU").innerHTML="Graphics Processing Unit";
+}
+function RAM(){
+	document.getElementById("RAM").innerHTML="Random Access Memory";
+}
+function ROM(){
+	document.getElementById("ROM").innerHTML="Read-Only Memory";
+}
+function HDD(){
+	document.getElementById("HDD").innerHTML="Hard Disk Drive";
+}
+function SSD(){
+	document.getElementById("SSD").innerHTML="Solid State Drive";
+}
+function USB(){
+	document.getElementById("USB").innerHTML="Universal Serial Bus";
+}
+function LAN(){
+	document.getElementById("LAN").innerHTML="Local Area Network";
+}
+function WAN(){
+	document.getElementById("WAN").innerHTML="Wide Area Network";
+}
+function WLAN(){
+	document.getElementById("WLAN").innerHTML="Wireless Local Area Network";
+}
+function ISP(){
+	document.getElementById("ISP").innerHTML="Internet Service Provider";
+}
+function DNS(){
+	document.getElementById("DNS").innerHTML="Domain Name System";
+}
+function HTTP(){
+	document.getElementById("HTTP").innerHTML="Hypertext Transfer Protocol";
+}
+function HTTPS(){
+	document.getElementById("HTTPS").innerHTML="Hypertext Transfer Protocol Secure";
+}
+function FTP(){
+	document.getElementById("FTP").innerHTML="File Transfer Protocol";
+}
+function TCP(){
+	document.getElementById("TCP").innerHTML="Transmission Control Protocol";
+}
+function IP(){
+	document.getElementById("IP").innerHTML="Internet Protocol";
+}
+function SMTP(){
+	document.getElementById("SMTP").innerHTML="Simple Mail Transfer Protocol";
+}
+function IMAP(){
+	document.getElementById("IMAP").innerHTML="Internet Message Access Protocol";
+}
+function POP(){
+	document.getElementById("POP").innerHTML="Post Office Protocol";
+}
+function VPN(){
+	document.getElementById("VPN").innerHTML="Virtual Private Network";
+}
+function HTML(){
+	document.getElementById("HTML").innerHTML="Hypertext Markup Language";
+}
+function CSS(){
+	document.getElementById("CSS").innerHTML="Cascading Style Sheets";
+}
+function PHP(){
+	document.getElementById("PHP").innerHTML="Hypertext Preprocessor";
+}
+function SQL(){
+	document.getElementById("SQL").innerHTML="Structured Query Language";
+}
+function IDE(){
+	document.getElementById("IDE").innerHTML="Integrated Development Environment";
+}
+function API(){
+	document.getElementById("API").innerHTML="Application Programming Interface";
+}
+function SDK(){
+	document.getElementById("SDK").innerHTML="Software Development Kit";
+}
+function GUI(){
+	document.getElementById("GUI").innerHTML="Graphical User Interface";
+}
+function CLI(){
+	document.getElementById("CLI").innerHTML="Command-Line Interface";
+}
+function UI(){
+	document.getElementById("UI").innerHTML="User Interface";
+}
+function UX(){
+	document.getElementById("UX").innerHTML="User Experience";
+}
+function OS(){
+	document.getElementById("OS").innerHTML="Operating System";
+}
+function BIOS(){
+	document.getElementById("BIOS").innerHTML="Basic Input/Output System";
+}
+function UEFI(){
+	document.getElementById("UEFI").innerHTML="Unified Extensible Firmware Interface";
+}
+function ACPI(){
+	document.getElementById("ACPI").innerHTML="Advanced Configuration and Power Interface";
+}
+function SATA(){
+	document.getElementById("SATA").innerHTML="Serial Advanced Technology Attachment";
+}
+function RAID(){
+	document.getElementById("RAID").innerHTML="Redundant Array of Independent Disks";
+}
+function DNSSEC(){
+	document.getElementById("DNSSEC").innerHTML="Domain Name System Security Extensions";
+}
+function SFTP(){
+	document.getElementById("SFTP").innerHTML="Secure File Transfer Protocol";
+}
+function SSH(){
+	document.getElementById("SSH").innerHTML="Secure Shell";
+}
+function SSL(){
+	document.getElementById("SSL").innerHTML="Secure Sockets Layer";
+}
+function TLS(){
+	document.getElementById("TLS").innerHTML="Transport Layer Security";
+}
+function VOIP(){
+	document.getElementById("VOIP").innerHTML="Voice over Internet Protocol";
+}
+function VLAN(){
+	document.getElementById("VLAN").innerHTML="Virtual Local Area Network";
+}
+function NAT(){
+	document.getElementById("NAT").innerHTML="Network Address Translation";
+}
+function DHCP(){
+	document.getElementById("DHCP").innerHTML="Dynamic Host Configuration Protocol";
+}
+function DNSBL(){
+	document.getElementById("DNSBL").innerHTML="Domain Name System Blacklist";
+}
+function SMTPS(){
+	document.getElementById("SMTPS").innerHTML="Simple Mail Transfer Protocol Secure";
+}
+function XMPP(){
+	document.getElementById("XMPP").innerHTML="Extensible Messaging and Presence Protocol";
+}
+function JSON(){
+	document.getElementById("JSON").innerHTML="JavaScript Object Notation";
+}
+function XML(){
+	document.getElementById("XML").innerHTML="Extensible Markup Language";
+}
+function AJAX(){
+	document.getElementById("AJAX").innerHTML="Asynchronous JavaScript and XML";
+}
+function APIPA(){
+	document.getElementById("APIPA").innerHTML="Automatic Private IP Addressing";
+}
+function MAC(){
+	document.getElementById("MAC").innerHTML="Media Access Control";
+}
+function MIME(){
+	document.getElementById("MIME").innerHTML="Multipurpose Internet Mail Extensions";
+}
+function NTP(){
+	document.getElementById("NTP").innerHTML="Network Time Protocol";
+}
+function POP3(){
+	document.getElementById("POP3").innerHTML="Post Office Protocol version 3";
+}
